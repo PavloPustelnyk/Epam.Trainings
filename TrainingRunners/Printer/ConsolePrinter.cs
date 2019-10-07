a442e8f1efd958f28c8b6b62cb2758d54b65dd15
pavlo.pustelnyk <pustelnyk.pavlo@gmail.com>
2019-10-07 18:50:10 +0300

    Add realization of IPrinter to write or read from System.Console

---
+++ TrainingRunners/Printer/ConsolePrinter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Epam.TrainingRunners
+{
+    public class ConsolePrinter : IPrinter
+    {
+        public string ReadLine() => Console.ReadLine();
+
+        public void Write(string s) => Console.Write(s);
+
+        public void WriteLine(string s) => Console.WriteLine(s);
+    }
+}
