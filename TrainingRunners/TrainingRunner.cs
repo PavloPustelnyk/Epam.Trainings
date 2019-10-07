97f0e98482a5c784dac74b57d5257bbac0965603
pavlo.pustelnyk <pustelnyk.pavlo@gmail.com>
2019-10-07 18:51:05 +0300

    Add TrainingRunner abstract class with method Run() and field IPrinter.

---
+++ TrainingRunners/TrainingRunner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Epam.TrainingRunners
+{
+    public abstract class TrainingRunner
+    {
+        protected IPrinter _printer;
+
+        public TrainingRunner(IPrinter printer)
+        {
+            _printer = printer;
+        }
+
+        public abstract void Run();
+    }
+}
