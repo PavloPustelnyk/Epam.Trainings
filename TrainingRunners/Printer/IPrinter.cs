ecedacc36d17307ebb1ab0f992a63663bbe28a08
pavlo.pustelnyk <pustelnyk.pavlo@gmail.com>
2019-10-07 18:49:05 +0300

    Add IPrinter interface with methods Write(), WriteLine(), ReadLine().

---
+++ TrainingRunners/Printer/IPrinter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Epam.TrainingRunners
+{
+    public interface IPrinter
+    {
+        void WriteLine(string s);
+        void Write(string s);
+        string ReadLine();
+    }
+}
