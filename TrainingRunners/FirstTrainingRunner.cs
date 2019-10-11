9fa358bd7e2b5057f777107cc2d0d7abf1237dd9
pavlo.pustelnyk <pustelnyk.pavlo@gmail.com>
2019-10-11 11:56:38 +0300

    Modifty runners output.

--- TrainingRunners/FirstTrainingRunner.cs
+++ TrainingRunners/FirstTrainingRunner.cs
@@ -15,11 +15,11 @@ namespace Epam.TrainingRunners
         public override void Run()
         {
             _printer.Clear();
-            _printer.WriteLine("\n< TRAINING 1 >\n");
+            _printer.WriteLine("\nTRAINING 1: Structs and Enums\n");
             try
             {
-                Task1();
-                Task2();
+                StructsTask();
+                EnumsTask();
             }
             catch(ArgumentException exc)
             {
@@ -29,48 +29,45 @@ namespace Epam.TrainingRunners
             {
                 _printer.WriteLine("\n Input error: " + exc.Message);
             }
+
+            _printer.WriteLine("\nPress any key to continue...");
+            _printer.ReadLine();
         }
 
-        private void Task1()
+        private void StructsTask()
         {
-            _printer.WriteLine("TASK 1\n");
+            _printer.WriteLine("Task 1: Person and Rectangle Structs\n");
+            PersonTask();
+            RectangleTask();
+        }
 
+        private void EnumsTask()
+        {
+            _printer.WriteLine("\nTask 2: Enums");
+            MonthsTask();
+            ColorsTask();
+            LongRangeValuesTask();
+        }
+
+        #region StructTasks
+
+        private void RectangleTask()
+        {
+            _printer.WriteLine("\n Rectangle Struct:");
+            ReadRectangle(out Rectangle rectangle);
+
+            _printer.WriteLine($" Result: Perimeter = {rectangle.Perimeter()}");
+        }
+
+        private void PersonTask()
+        {
+            _printer.WriteLine(" Person Struct:\n");
             ReadPerson(out Person person);
 
             _printer.Write(" Age to compare: ");
             int age = int.Parse(_printer.ReadLine());
 
             _printer.WriteLine(" Result: " + person.OlderThan(age));
-
-            ReadRectangle(out Rectangle rectangle);
-
-            _printer.WriteLine($" Result: Perimeter = {rectangle.Perimeter()}");
-        }
-
-        private void Task2()
-        {
-            _printer.WriteLine("\n\nTASK 2\n");
-
-            int n;
-            _printer.Write(" Enter n: ");
-            if (int.TryParse(_printer.ReadLine(), out n) && n >= 0 && n < 12)
-            {
-                _printer.WriteLine(" " + Enum.GetName(typeof(Months), n));
-            }
-            else
-            {
-                _printer.WriteLine(" Wrong input.");
-            }
-
-            Colors color = Colors.Black;
-            _printer.WriteLine(" \nColors: " + color.Values());
-
-            var longRangeValues = Enum.GetValues(typeof(LongRange));
-            _printer.WriteLine(" \nLongRange: ");
-            foreach (var a in longRangeValues)
-            {
-                _printer.Write($" {a} = {(long)a};");
-            }
         }
 
         private void ReadPerson(out Person person)
@@ -111,5 +108,46 @@ namespace Epam.TrainingRunners
 
             rectangle = new Rectangle { X = x, Y = y, Width = width, Height = height };
         }
+
+        #endregion
+
+        #region EnumTasks
+
+        private void LongRangeValuesTask()
+        {
+            _printer.WriteLine("\n LongRangeValues Enum:");
+            var longRangeValues = Enum.GetValues(typeof(LongRange));
+            _printer.WriteLine(" \n LongRange: ");
+            foreach (var a in longRangeValues)
+            {
+                _printer.Write($" {a} = {(long)a};");
+            }
+            _printer.WriteLine();
+        }
+
+        private void ColorsTask()
+        {
+            _printer.WriteLine("\n Colors Enum: ");
+            Colors color = Colors.Black;
+            _printer.WriteLine(" \n Colors: " + color.Values());
+        }
+
+        private void MonthsTask()
+        {
+            _printer.WriteLine("\n Months Enum:\n");
+            int n;
+            _printer.Write(" Enter n: ");
+            if (int.TryParse(_printer.ReadLine(), out n) && n >= 0 && n < 12)
+            {
+                _printer.WriteLine(" Month: " + Enum.GetName(typeof(Months), n));
+            }
+            else
+            {
+                _printer.WriteLine(" Wrong input.");
+            }
+        }
+
+        #endregion
+
     }
 }
--- TrainingRunners/SecondTrainingRunner.cs
+++ TrainingRunners/SecondTrainingRunner.cs
@@ -14,42 +14,71 @@ namespace Epam.TrainingRunners
         public override void Run()
         {
             _printer.Clear();
-            _printer.WriteLine("\n< TRAINING 2 >\n");
+            _printer.WriteLine("\nTRAINING 2: Exceptions\n");
 
+            OverflowTask();
+            IndexOutOfRangeTask();
+            ArgumentExcTask();
+
+            _printer.WriteLine("\nPress any key to continue...");
+            _printer.ReadLine();
+        }
+
+        #region ExceptionTasks
+
+        private void ArgumentExcTask()
+        {
             try
             {
-                //ExceptionTests.StackOverflow();
-            }
-            catch(StackOverflowException exc)
-            {
-                _printer.WriteLine(exc.Message);
-            }
-
-            try
-            {
-                ExceptionTests.IndexOutOfRange();
-            }
-            catch(IndexOutOfRangeException exc)
-            {
-                _printer.WriteLine(exc.Message);
-            }
-
-            try
-            {
+                _printer.WriteLine("\nTask 3: ArgumentException");
                 ExceptionTests.DoSomeMath(-1, 2);
             }
-            catch(ArgumentException exc)
+            catch (ArgumentException exc)
             {
                 _printer.WriteLine(exc.Message);
-                if(exc.ParamName == "a")
+                if (exc.ParamName == "a")
                 {
                     _printer.Write("Change parameter A.");
                 }
-                else if(exc.ParamName == "b")
+                else if (exc.ParamName == "b")
                 {
                     _printer.Write("Change parameter B.");
                 }
             }
         }
+
+        private void IndexOutOfRangeTask()
+        {
+            try
+            {
+                _printer.WriteLine("\nTask 2: IndexoutOfRangeException");
+                ExceptionTests.IndexOutOfRange();
+            }
+            catch (IndexOutOfRangeException exc)
+            {
+                _printer.WriteLine(exc.Message);
+            }
+        }
+
+        private void OverflowTask()
+        {
+            try
+            {
+                _printer.WriteLine("\nTask 1: StackOverflowException");
+
+                bool shouldContinue = false;
+                _printer.Write("\nGenerate StackOverflowException? (y/n): ");
+                if (_printer.ReadLine() == "y")
+                {
+                    ExceptionTests.StackOverflow();
+                }
+            }
+            catch (StackOverflowException exc)
+            {
+                _printer.WriteLine(exc.Message);
+            }
+        }
+
+        #endregion
     }
 }
