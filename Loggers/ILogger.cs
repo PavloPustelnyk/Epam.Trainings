2f061508f889f8e6fd9988971a3062f26dc97d73
pavlo.pustelnyk <pustelnyk.pavlo@gmail.com>
2019-10-15 13:14:43 +0300

    Add ILogger interface with LogMessage() and DumpLog().

---
+++ Loggers/ILogger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Epam.Loggers
+{
+    public interface ILogger
+    {
+        bool LogMessage(string message);
+        IEnumerable<string> DumpLog();
+    }
+}
