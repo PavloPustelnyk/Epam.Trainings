9ac0525e2467ae0578cdc079960aa7be01d5c5dd
pavlo.pustelnyk <pustelnyk.pavlo@gmail.com>
2019-10-15 13:15:34 +0300

    Add IWriter interface with Write(), WriteLine(), Clear().

---
+++ OutputWriters/IWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Epam.Writers
+{
+    public interface IWriter
+    {
+        void WriteLine(string s = "");
+        void Write(string s);
+        void Clear();
+    }
+}
