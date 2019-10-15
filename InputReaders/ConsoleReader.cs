17357c0bcc248a4ff6edf0f37a5b8e3e6f055502
pavlo.pustelnyk <pustelnyk.pavlo@gmail.com>
2019-10-15 13:13:45 +0300

    Add ConsoleReader with ReadLine().

---
+++ InputReaders/ConsoleReader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Epam.Readers
+{
+    public class ConsoleReader : IReader
+    {
+        public string ReadLine() => Console.ReadLine();
+    }
+}
