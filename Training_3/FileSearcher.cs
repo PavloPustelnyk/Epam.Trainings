c11271b0bbea9ed97716b553f5db3a7f5bedc70c
pavlo.pustelnyk <pustelnyk.pavlo@gmail.com>
2019-10-15 13:19:16 +0300

    Add FileSearcher class with Training 3 Task 2 solution.

---
+++ Training_3/FileSearcher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Epam.Training_3.Task_2
+{
+    public class FileSearcher
+    {
+        public IEnumerable<string> FindFileByPattern(string pattern, bool shouldSearchInSubdirs = false, string directory = "")
+        {
+            if (String.IsNullOrEmpty(pattern))
+            {
+                throw new ArgumentException("Search pattern cannot be empty.");
+            }
+
+            if(!String.IsNullOrEmpty(directory) && !Directory.Exists(directory))
+            {
+                throw new ArgumentException("Wrong directory.");
+            }
+            else if(String.IsNullOrEmpty(directory))
+            {
+                directory = Directory.GetCurrentDirectory();
+            }
+
+            return shouldSearchInSubdirs ?
+                Directory.GetFiles(directory, pattern, SearchOption.AllDirectories) : 
+                Directory.GetFiles(directory, pattern, SearchOption.TopDirectoryOnly);
+        }
+    }
+}
