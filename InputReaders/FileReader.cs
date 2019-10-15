f14d1ab4c51a995366b67e5c562d41d8762715c5
pavlo.pustelnyk <pustelnyk.pavlo@gmail.com>
2019-10-15 13:14:09 +0300

    Add FileReader.

---
+++ InputReaders/FileReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Epam.Readers
+{
+    public class FileReader : IReader
+    {
+        public string FullFileName { get; set; }
+
+        public FileReader(string fullFileName)
+        {
+            FullFileName = fullFileName;
+        }
+
+        public string ReadLine()
+        {
+            using (StreamReader reader = new StreamReader(FullFileName))
+            {
+                return reader.ReadLine();
+            }
+        }
+
+        public IEnumerable<string> ReadAll()
+        {
+            return File.ReadAllLines(FullFileName);
+        }
+    }
+}
