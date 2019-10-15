b3be5e968850854c7c7faede1daae0f0c09f4ab0
pavlo.pustelnyk <pustelnyk.pavlo@gmail.com>
2019-10-15 13:16:11 +0300

    Add FileWriter that implements IWriter.

---
+++ OutputWriters/FileWriter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Epam.Writers
+{
+    public class FileWriter : IWriter
+    {
+        public string FullFileName { get; set; }
+
+        public FileWriter(string fullFileName)
+        {
+            FullFileName = fullFileName;
+        }
+
+        public static bool CanCreateFile(string fullFileName)
+        {
+            bool canCreate = false;
+            try
+            {
+                using (File.Create(fullFileName)) { }
+                File.Delete(fullFileName);
+                canCreate = true;
+            }
+            catch
+            {
+                canCreate = false;
+            }
+            return canCreate;
+        }
+
+        public static bool FileExists(string fullFileName)
+        {
+            return File.Exists(fullFileName);
+        }
+
+        public void Clear()
+        {
+            using (StreamWriter log = File.CreateText(FullFileName))
+            {
+                log.Write(String.Empty);
+            }
+        }
+
+        public void Write(string s)
+        {
+            using (StreamWriter log = File.AppendText(FullFileName))
+            {
+                log.Write(s);
+            }
+        }
+
+        public void WriteLine(string s = "")
+        {
+            using (StreamWriter log = File.AppendText(FullFileName))
+            {
+                log.WriteLine(s);
+            }
+        }
+    }
+}
