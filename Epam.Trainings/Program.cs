0cb631b32efd1087b9d83bba159bc3bf891c38ec
pavlo.pustelnyk <pustelnyk.pavlo@gmail.com>
2019-10-15 13:12:37 +0300

    Add ThirdTrainingRunner to runners.

--- Epam.Trainings/Program.cs
+++ Epam.Trainings/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using Epam.TrainingRunners;
+using Epam.Readers;
+using Epam.Writers;
+using Epam.Loggers;
 
 namespace Epam.Trainings
 {
@@ -8,14 +11,33 @@ namespace Epam.Trainings
     {
         static void Main(string[] args)
         {
-            var runners = new List<TrainingRunner>
+            try
             {
-                new FirstTrainingRunner(new ConsolePrinter()),
-                new SecondTrainingRunner(new ConsolePrinter())
-            };
+                var runners = new List<ITrainingRunner>
+                {
+                    /*new FirstTrainingRunner
+                    {
+                        Writer = new ConsoleWriter(),
+                        Reader = new ConsoleReader()
+                    },
+                    new SecondTrainingRunner
+                    {
+                        Writer = new ConsoleWriter(),
+                        Reader = new ConsoleReader()
+                    },*/
+                    new ThirdTrainingRunner
+                    {
+                        Writer = new ConsoleWriter(),
+                        Reader = new ConsoleReader()
+                    }
+                };
 
-            runners.ForEach(r => r.Run());
-
+                runners.ForEach(r => r.Run());
+            }
+            catch(Exception exc)
+            {
+                Console.WriteLine($"\n\n{exc.Message}");
+            }
             Console.ReadLine();
         }
     }
