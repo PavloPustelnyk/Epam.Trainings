6114ae0a1740d04c6b1d1283399c2c6225ceb7ca
pavlo.pustelnyk <pustelnyk.pavlo@gmail.com>
2019-10-15 13:15:00 +0300

    Add FileLogger that implements ILogger.

---
+++ Loggers/FileLogger.cs
@@ -0,0 +1,81 @@
+﻿using Epam.Writers;
+using System.Collections.Generic;
+using Epam.Readers;
+using System;
+using System.IO;
+
+namespace Epam.Loggers
+{
+    public class FileLogger : ILogger
+    {
+        protected FileWriter Writer { get; set; }
+
+        protected FileReader Reader { get; set; }
+
+        public string FullFileName { get; set; }
+
+        public FileLogger(string fullFileName = "log.txt")
+        {
+            if(!ChangeFile(fullFileName))
+            {
+                throw new ArgumentException("Wrong log file name. Check file path.");
+            }
+        }
+
+        public bool ChangeFile(string fullFileName)
+        {
+            bool canChange = false;
+            if(String.IsNullOrEmpty(fullFileName) || 
+                (!FileWriter.FileExists(fullFileName) && 
+                !FileWriter.CanCreateFile(fullFileName)))
+            {
+                return false;
+            }
+            try
+            {
+                FullFileName = fullFileName;
+                Writer = new FileWriter(fullFileName);
+                Reader = new FileReader(fullFileName);
+                canChange = true;
+            }
+            catch
+            {
+                canChange = false;
+            }
+            return canChange;
+        }
+
+        public bool LogMessage(string message)
+        {
+            bool isOk = false;
+            try
+            {
+                Writer.WriteLine($"{DateTime.Now.ToLongTimeString()} " +
+                    $"{DateTime.Now.ToLongDateString()}   {message}");
+                isOk = true;
+            }
+            catch
+            {
+                isOk = false;
+            }
+            return isOk;
+        }
+
+        public IEnumerable<string> DumpLog()
+        {
+            IEnumerable<string> log;
+            try
+            {
+                log = Reader.ReadAll();
+                Writer.Clear();
+            }
+            catch
+            {
+                log = null;
+            }
+            return log;
+        }
+
+
+    }
+}
