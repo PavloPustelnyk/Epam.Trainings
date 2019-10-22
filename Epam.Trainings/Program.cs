698b4f6a942167818f2edde124b5c4f52ceabd11
pavlo.pustelnyk <pustelnyk.pavlo@gmail.com>
2019-10-22 13:50:40 +0300

    Add custom logger.

--- Epam.Trainings/Program.cs
+++ Epam.Trainings/Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using Epam.TrainingRunners;
 using Epam.Readers;
 using Epam.Writers;
-using Epam.Loggers;
+using Epam.Logger;
+using Epam.Logger.Configurators;
+using Microsoft.Extensions.Configuration;
+using NLog;
 
 namespace Epam.Trainings
 {
@@ -11,24 +14,41 @@ namespace Epam.Trainings
     {
         static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", true, true)
+                    .Build();
+
+            string logFile = configuration["DefaultLogFileName"];
+
+            LoggerFactory logFactory = new LoggerFactory();
+            LoggerConfigurator loggerConfigurator = new LoggerConfigurator();
+            if (!string.IsNullOrEmpty(logFile))
+            {
+                loggerConfigurator.AddWriter(new FileWriter(logFile));
+            }
+            var logger = logFactory.GetLogger(loggerConfigurator);
+
             try
             {
                 var runners = new List<ITrainingRunner>
                 {
-                    /*new FirstTrainingRunner
+                    new FirstTrainingRunner
                     {
                         Writer = new ConsoleWriter(),
-                        Reader = new ConsoleReader()
+                        Reader = new ConsoleReader(),
+                        Logger = logger
                     },
                     new SecondTrainingRunner
                     {
                         Writer = new ConsoleWriter(),
-                        Reader = new ConsoleReader()
-                    },*/
+                        Reader = new ConsoleReader(),
+                        Logger = logger
+                    },
                     new ThirdTrainingRunner
                     {
                         Writer = new ConsoleWriter(),
-                        Reader = new ConsoleReader()
+                        Reader = new ConsoleReader(),
+                        Logger = logger
                     }
                 };
 
@@ -37,6 +57,7 @@ namespace Epam.Trainings
             catch(Exception exc)
             {
                 Console.WriteLine($"\n\n{exc.Message}");
+                logger.LogMessage(exc.Message);
             }
             Console.ReadLine();
         }
