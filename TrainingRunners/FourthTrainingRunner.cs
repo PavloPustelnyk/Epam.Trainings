a534540ba81788aae1aaf76d2751eb4131c51944
pavlo.pustelnyk <pustelnyk.pavlo@gmail.com>
2019-10-24 18:55:17 +0300

    Add fourth training runner.

--- TrainingRunners/FourthTrainingRunner.cs
+++ TrainingRunners/FourthTrainingRunner.cs
@@ -3,21 +3,33 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
 using Epam.Trainings.Logger;
+using Epam.Trainings.Training_4;
+using Epam.Trainings.Writers;
+using Epam.Trainings.Readers;
 
 namespace Epam.Trainings.TrainingRunners
 {
     public class FourthTrainingRunner : ITrainingRunner
     {
+        public IWriter Writer { get; set; }
+        public IReader Reader { get; set; }
         public ILogger Logger { get; set; }
 
         public void Run()
         {
-            DisplayAssemblyInfo();
+            Writer.Clear();
+            Writer.WriteLine("\nTRAINING 4: Reflection\n");
+
+            DisplayExecutionAssemblyInfo();
+
+            Writer.WriteLine("\nPress any key to continue...");
+            Reader.ReadLine();
         }
 
-        private void DisplayAssemblyInfo()
+        private void DisplayExecutionAssemblyInfo()
         {
-            Assembly.GetEntryAssembly().DefinedTypes;
+            var assemblyVisualizer = new AssemblyVisualizer { Writer = this.Writer };
+            assemblyVisualizer.DisplayAssemblyInfo(Assembly.GetExecutingAssembly());
         }
     }
 }
