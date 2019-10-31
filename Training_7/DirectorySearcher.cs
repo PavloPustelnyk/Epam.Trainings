26d10ed409dc28a0417a81f60910db2e37f2a71a
pavlo.pustelnyk <pustelnyk.pavlo@gmail.com>
2019-10-31 17:24:22 +0200

    Add project with DirectorySearcher and ExcelListSearcher classes for Training 7.

---
+++ Training_7/DirectorySearcher.cs
@@ -0,0 +1,66 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DirectorySearcher.cs" company="Epam">
+//     Copyright (c) Epam. All rights reserved.
+// </copyright>
+// <author>Pavlo Pustelnyk</author>
+//-----------------------------------------------------------------------
+namespace Epam.Trainings.Training_7
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    /// <summary>
+    /// The main DirectorySearcher class.
+    /// Contains methods to get list of duplicate or unique files of two directories.
+    /// </summary>
+    public class DirectorySearcher
+    {
+        /// <summary>
+        /// Searches files that are in both first and second directory and subdirectories.
+        /// </summary>
+        /// <param name="firstDirectory">DirectoryInfo about first directory</param>
+        /// <param name="secondDirectory">DirectoryInfo about second directory</param>
+        /// <returns>
+        /// List of strings - names of files that are in both first and second directories and subdirectories.
+        /// </returns>
+        public List<string> GetDuplicateFiles(DirectoryInfo firstDirectory, DirectoryInfo secondDirectory)
+        {
+            if (firstDirectory == null || !firstDirectory.Exists
+                || secondDirectory == null || !secondDirectory.Exists)
+            {
+                throw new ArgumentException($"Wrong directory.");
+            }
+
+            var firstDirFiles = firstDirectory.EnumerateFiles("*", SearchOption.AllDirectories)
+                                              .Select(f => f.Name);
+            var secondDirFiles = secondDirectory.EnumerateFiles("*", SearchOption.AllDirectories)
+                                                .Select(f => f.Name);
+            return firstDirFiles.Intersect(secondDirFiles).ToList();
+        }
+
+        /// <summary>
+        /// Searches files that are unique between first and second directory and subdirectories.
+        /// </summary>
+        /// <param name="firstDirectory">DirectoryInfo about first directory</param>
+        /// <param name="secondDirectory">DirectoryInfo about second directory</param>
+        /// <returns>
+        /// List of strings - names of files that are unique between first and second directories and subdirectories.
+        /// </returns>
+        public List<string> GetUniqueFiles(DirectoryInfo firstDirectory, DirectoryInfo secondDirectory)
+        {
+            if (firstDirectory == null || !firstDirectory.Exists
+                || secondDirectory == null || !secondDirectory.Exists)
+            {
+                throw new ArgumentException($"Wrong directory.");
+            }
+
+            var firstDirFiles = firstDirectory.EnumerateFiles("*", SearchOption.AllDirectories)
+                                              .Select(f => f.Name);
+            var secondDirFiles = secondDirectory.EnumerateFiles("*", SearchOption.AllDirectories)
+                                                .Select(f => f.Name);
+            return firstDirFiles.Except(secondDirFiles).Union(secondDirFiles.Except(firstDirFiles)).ToList();
+        }
+    }
+}
---
+++ Training_7/ExcelListSearcher.cs
@@ -0,0 +1,119 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ExcelListSearcher.cs" company="Epam">
+//     Copyright (c) Epam. All rights reserved.
+// </copyright>
+// <author>Pavlo Pustelnyk</author>
+//-----------------------------------------------------------------------
+namespace Epam.Trainings.Training_7
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Microsoft.Extensions.Configuration;
+    using Syncfusion.XlsIO;
+
+    /// <summary>
+    /// Class that contains method to get unique values between two lists in Excel
+    /// </summary>
+    public class ExcelListSearcher
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExcelListSearcher" /> class.
+        /// Creates configuration for settings file.
+        /// </summary>
+        public ExcelListSearcher()
+        {
+            this.Configuration = new ConfigurationBuilder()
+                                .AddJsonFile("appsettings.json", true, true)
+                                .Build();
+        }
+
+        /// <summary>
+        /// Gets or sets Configuration.
+        /// </summary>
+        private IConfigurationRoot Configuration { get; set; }
+
+        /// <summary>
+        /// Find all unique values between two lists in Excel file.
+        /// Considers all values from specified start line to first empty cell.
+        /// </summary>
+        /// <param name="fullExcelFileName">Full name of Excel file</param>
+        /// <param name="firstListColumn">Column of first list in Excel file</param>
+        /// <param name="firstListStartLine">Start line of first list in Excel file</param>
+        /// <param name="secondListColumn">Column of second list in Excel file</param>
+        /// <param name="secondListStartLine">Start line of second list in Excel file</param>
+        /// <returns>List of unique values between two lists in Excel file</returns>
+        public List<string> GetUniqueColumnsBetweenLists(
+            string fullExcelFileName, 
+            string firstListColumn, 
+            int firstListStartLine, 
+            string secondListColumn, 
+            int secondListStartLine)
+        {
+            if (string.IsNullOrEmpty(fullExcelFileName))
+            {
+                throw new ArgumentException("Excel file name cannot be empty.");
+            }
+
+            List<string> result = new List<string>();
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Excel2013;
+                IWorkbook workbook;
+
+                using (FileStream fs = new FileStream(fullExcelFileName, FileMode.Open))
+                {
+                    workbook = excelEngine.Excel.Workbooks.Open(fs);
+                }
+
+                if (workbook == null)
+                {
+                    throw new ArgumentException("Wrong excel file name.");
+                }
+
+                application.UseFastRecordParsing = true;
+
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                List<string> firstList = this.GetItemsFromRange(
+                    worksheet, 
+                    firstListColumn, 
+                    firstListStartLine);
+                List<string> secondList = this.GetItemsFromRange(
+                    worksheet, 
+                    secondListColumn,
+                    secondListStartLine);
+
+                result = firstList.Except(secondList).Union(secondList.Except(firstList)).ToList();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Method to get all values for specified column from start line to first empty cell.
+        /// </summary>
+        /// <param name="worksheet">Excel Worksheet object</param>
+        /// <param name="column">Column to get values</param>
+        /// <param name="line">Start line</param>
+        /// <returns>List of values for specified column from start line to first empty cell</returns>
+        private List<string> GetItemsFromRange(IWorksheet worksheet, string column, int line)
+        {
+            if (worksheet == null)
+            {
+                throw new ArgumentException("Worksheet cannot be null.");
+            }
+
+            List<string> result = new List<string>();
+            while (!string.IsNullOrEmpty(worksheet.Range[column + line].Value))
+            {
+                result.Add(worksheet.Range[column + line].Value);
+                ++line;
+            }
+
+            return result;
+        }
+    }
+}
