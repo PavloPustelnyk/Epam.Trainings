d74c9a9a7cd904cd6cc6041363bb84b72b671e71
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2019-11-26 16:12:42 +0200

    Add services for IoC Container tests.

---
+++ Epam.Training.Tests/TestServices.cs
@@ -0,0 +1,44 @@
+﻿namespace Epam.Training.Tests
+{
+    public interface IRepository<T>
+    {
+        public int a { get; set; }
+    }
+
+    public class Repository<T> : IRepository<T>
+    {
+        public Repository(ILogger logger)
+        {
+
+        }
+
+        public int a { get; set; } = 1;
+    }
+
+    public class InvoiceService
+    {
+        public InvoiceService(IRepository<Customer> repository, ILogger logger)
+        {
+
+        }
+    }
+
+    public class Customer
+    {
+
+    }
+
+    public class ILogger
+    {
+
+    }
+
+    public class Logger : ILogger
+    {
+        public Logger()
+        {
+
+        }
+
+    }
+}
